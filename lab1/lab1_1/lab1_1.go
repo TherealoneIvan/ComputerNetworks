d2b6a6b8c9c318897a0a83d368434a239cf6041b
Steven <Steven@DESKTOP-02NBP64>
2019-09-18 00:55:38 +0300

    лаба 1.1 реализация функций, указанных в задании

--- lab1/lab1_1/lab1_1.go
+++ lab1/lab1_1/lab1_1.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"           // пакет для форматированного ввода вывода
 	"html/template" // пакет для шаблонизации HTML документов
-	"log"           // пакет для логирования
-	"net/http"      // пакет для поддержки HTTP протокола
-	"sort"          // пакет для сортировки
-	"strings"       // пакет для работы с  UTF-8 строками
-	"time"          // пакет для работы со временем
+	"io"
+	"log"      // пакет для логирования
+	"net/http" // пакет для поддержки HTTP протокола
+	"sort"     // пакет для сортировки
+	"strings"  // пакет для работы с  UTF-8 строками
+	"time"     // пакет для работы со временем
 
 	"github.com/jlaffaye/ftp" //пакет для написания FTP клиента
 )
@@ -22,6 +24,12 @@ type EntryExtended struct {
 type EntriesPageData struct {
 	EntriesList []EntryExtended
 	CurrentDir  string
+}
+
+// ErrorPageData data for error page
+type ErrorPageData struct {
+	Message     string
+	RedirectURL string
 }
 
 // EntryTypeToString : Преобразует тип вхождения элемента ftp сервера к строке
@@ -58,25 +66,39 @@ func EntriesByType(path string, conn *ftp.ServerConn, etype ftp.EntryType) []Ent
 	return filteredEntries
 }
 
-// HomeRouterHandler : router
-func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
+// ErrorHandling : Возврат клиенту информации о произошедшей ошибке
+func ErrorHandling(e error, path string, w http.ResponseWriter) {
+	tmplt := template.Must(template.ParseFiles("static/error.html"))
+	data := ErrorPageData{
+		Message:     e.Error(),
+		RedirectURL: path}
+	tmplt.Execute(w, data) // отправляем данные на клиентскую сторону
+}
+
+// FTPAuth : Авторизация на FTP сервере
+func FTPAuth(path string, w http.ResponseWriter) *ftp.ServerConn {
 	c, err := ftp.Dial("students.yss.su:21", ftp.DialWithTimeout(5*time.Second)) // соединение с ftp сервером
 	if err != nil {
-		fmt.Println(err)
+		ErrorHandling(err, path, w)
 	}
 
 	err = c.Login("ftpiu8", "3Ru7yOTA") // авторизация на сервере
 	if err != nil {
-		fmt.Println(err)
+		ErrorHandling(err, path, w)
 	}
 
-	r.ParseForm() // анализ аргументов
+	return c
+}
 
+// ClientRouterHandler : Обработчик запросов ко клиенту
+func ClientRouterHandler(w http.ResponseWriter, r *http.Request) {
 	path := strings.Replace(r.URL.Path, "/client", "", 1)
 	n := len(path)
 	if n > 1 && path[n-1] == '/' {
 		path = path[:n-1]
 	}
+
+	c := FTPAuth(path, w)
 
 	var entries []EntryExtended
 	entries = append(entries, EntriesByType(path, c, ftp.EntryTypeFolder)...) // папки
@@ -84,11 +106,12 @@ func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	entries = append(entries, EntriesByType(path, c, ftp.EntryTypeLink)...)   // ссылки
 
 	if err := c.Quit(); err != nil {
-		fmt.Println(err)
+		ErrorHandling(err, path, w)
+		return
 	}
 
 	if len(entries) == 0 {
-		fmt.Fprintf(w, "No such file or directory")
+		ErrorHandling(errors.New("No such file or directory"), path, w)
 	} else {
 		tmplt := template.Must(template.ParseFiles("static/index.html"))
 		data := EntriesPageData{
@@ -98,12 +121,169 @@ func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// CreateRouterHandler : Обработчик запроса на создание директории
+func CreateRouterHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // анализ аргументов
+
+	current := "" // текущая директория
+	dir := ""     // имя создаваемой директории
+
+	for k, v := range r.Form {
+		if k == "curdir" {
+			current = v[0]
+		} else if k == "dirname" {
+			dir = v[0]
+		}
+	}
+
+	c := FTPAuth(current, w)
+
+	err := c.ChangeDir(current)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	err = c.MakeDir(dir)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	if err := c.Quit(); err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	http.Redirect(w, r, strings.Join([]string{"client", current}, "/"), 301)
+}
+
+// UploadRouterHandler : Обработчик запроса на загрузку файла
+func UploadRouterHandler(w http.ResponseWriter, r *http.Request) {
+	var maxMB int64 = 10 // максимальный размер загружаемого файла в мегабайтах
+
+	r.ParseMultipartForm(maxMB << 20) // анализ аргументов
+
+	current := r.Form["curdir"][0] // директория загрузки
+	file, handler, err := r.FormFile("uploadedfile")
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+	defer file.Close()
+
+	c := FTPAuth(current, w)
+
+	err = c.ChangeDir(current)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	err = c.Stor(handler.Filename, file)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.Quit(); err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	http.Redirect(w, r, strings.Join([]string{"client", current}, "/"), 301)
+}
+
+// DownloadRouterHandler : Обработчик запроса на скачивание файла
+func DownloadRouterHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // анализ аргументов
+
+	current := "" // текущая директория
+	file := ""    // имя скачиваемого файла
+
+	for k, v := range r.Form {
+		if k == "curdir" {
+			current = v[0]
+		} else if k == "filename" {
+			file = v[0]
+		}
+	}
+
+	c := FTPAuth(current, w)
+
+	err := c.ChangeDir(current)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	reader, err := c.Retr(file) // считывающий поток, содержащий байты файла
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	// установка необходимых заголовков
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", file))
+
+	if err := c.Quit(); err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	io.Copy(w, reader) // отдать клиенту читающий поток без загрузки файла в память
+}
+
+// DeleteFileRouterHandler : Обработчик запроса на удаление файла
+func DeleteFileRouterHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // анализ аргументов
+
+	current := "" // текущая директория
+	file := ""    // имя удаляемого файла
+
+	for k, v := range r.Form {
+		if k == "curdir" {
+			current = v[0]
+		} else if k == "filename" {
+			file = v[0]
+		}
+	}
+
+	c := FTPAuth(current, w)
+
+	err := c.ChangeDir(current)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	err = c.Delete(file)
+	if err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	if err := c.Quit(); err != nil {
+		ErrorHandling(err, current, w)
+		return
+	}
+
+	http.Redirect(w, r, strings.Join([]string{"client", current}, "/"), 301)
+}
+
 func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css")))) // доступ к стилям
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/client", 301)
-	})
-	http.HandleFunc("/client/", HomeRouterHandler) // установим роутер
-	err := http.ListenAndServe(":8017", nil)       // задаём слушать порт
+	}) // редирект на action client с запроса по адресу веб-сервера
+
+	http.HandleFunc("/client/", ClientRouterHandler)
+	http.HandleFunc("/create", CreateRouterHandler)
+	http.HandleFunc("/upload", UploadRouterHandler)
+	http.HandleFunc("/download", DownloadRouterHandler)
+	http.HandleFunc("/deletefile", DeleteFileRouterHandler)
+
+	err := http.ListenAndServe(":8017", nil) // задаём слушать порт
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
