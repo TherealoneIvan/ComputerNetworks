b01270c0cfbafd35a88cda172d0d5be5a30a59c9
Steven <Steven@DESKTOP-02NBP64>
2019-09-18 17:04:00 +0300

    лаба 1.2 хардкод сервер

--- lab1/lab1_2/lab1_2.go
+++ lab1/lab1_2/lab1_2.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	filedriver "gitea.com/goftp/file-driver"
+	"goftp.io/server"
+)
 
+func main() {
+	var (
+		user = "admin"
+		pass = "123456"
+	)
+
+	factory := &filedriver.FileDriverFactory{
+		RootPath: "ftproot",
+		Perm:     server.NewSimplePerm("root", "root")}
+
+	opts := &server.ServerOpts{
+		Factory:  factory,
+		Port:     21,
+		Hostname: "185.20.227.83",
+		Auth:     &server.SimpleAuth{Name: user, Password: pass}}
+	server := server.NewServer(opts)
+	server.ListenAndServe()
 }
