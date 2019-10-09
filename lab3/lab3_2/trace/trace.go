e2736fd3572c58ecaf2fff0e349b23ada9b1821b
Stepami <iu9_32_17@lab.posevin.com>
2019-10-09 13:26:43 +0000

    трассировка

--- lab3/lab3_2/trace/trace.go
+++ lab3/lab3_2/trace/trace.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	var host string
 	flag.StringVar(&host, "host", "www.google.com", "host to trace")
-	out, err := exec.Command("tracert", host).Output()
+	out, err := exec.Command("traceroute", host).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
